e3255b6836572ccce8b8d2fe5d99c674fd0f7f27
KC Thomas <thisiskcthomas@gmail.com>
2020-11-17 13:37:06 +0000

    Min and max frequencies can be changed using #change_min_and_max_values

--- lib/bandpass.rb
+++ lib/bandpass.rb
@@ -2,24 +2,31 @@ class BandPass
 
   def initialize(frequencies)
     @frequencies = frequencies
+    @max_frequency = 1000
+    @min_frequency = 40
   end
   
   def filter_frequencies
     @frequencies.each.with_index do |frequency, index|
-      change_frequency(1000, index) if high_pass?(frequency)
-      change_frequency(40, index) if low_pass?(frequency)
+      change_frequency(@max_frequency, index) if high_pass?(frequency)
+      change_frequency(@min_frequency, index) if low_pass?(frequency)
     end
     @frequencies
+  end
+
+  def change_min_and_max_values(min, max)
+    @max_frequency = max
+    @min_frequency = min
   end
 
   private
 
   def high_pass?(frequency)
-    frequency > 1000
+    frequency > @max_frequency
   end
 
   def low_pass?(frequency)
-    frequency < 40
+    frequency < @min_frequency
   end
 
   def change_frequency(frequency, index)
--- spec/bandpass_spec.rb
+++ spec/bandpass_spec.rb
@@ -1,12 +1,17 @@
 require 'bandpass'
 
 describe BandPass do 
-  let(:frequencies) { [35, 39, 40, 68, 1200, 999, 78, 1001]}
+  let(:frequencies) { [15, 35, 39, 40, 68, 3000, 999, 78, 1001, 3200]}
   before :each do
     @bandpass = BandPass.new(frequencies)
   end
 
   it("returns an array of frequencies that do not go above the max default") do
-    expect(@bandpass.filter_frequencies).to eq([40, 40, 40, 68, 1000, 999, 78, 1000])
+    expect(@bandpass.filter_frequencies).to eq([40, 40, 40, 40, 68, 1000, 999, 78, 1000, 1000])
+  end
+
+  it("can change the max frequency and the min frequency") do
+    @bandpass.change_min_and_max_values(20, 3000) 
+    expect(@bandpass.filter_frequencies).to eq([20, 35, 39, 40, 68, 3000, 999, 78, 1001, 3000])
   end
 end
